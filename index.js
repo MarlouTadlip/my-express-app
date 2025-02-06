f66abc1fcbc0845ab6d0e15dc0a04ae4b696d8b4
MarlouTadlip <tadlipmarlou@gmail.com>
2025-02-06 23:22:19 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+})
+
+app.use((err,req,res,next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
 app.get('/', (req,res) => {
-    res.send('Hello World!');
+    res.send("Hello World!");
 })
 
 app.get('/about', (req,res) => {
-    res.send('About Us')
+    res.send("About Us")
 })
 
 app.post('/submit', (req,res) => {
-    const data = req.body
+    const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`)
 })
 
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req,res) => {
+    res.json(items)
+})
+
+app.post ('/items', (req,res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items)
+
+})
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
-})  
+    console.log(`Server running on http://localhost:${port}`);
+})
